d419480cc394fb1fa422b7cdc647271ddafc1551
KanchiMoe <github@chiyo-chan.me>
2024-07-06 00:04:22 +0100

    return struct

--- src/page_scraper.go
+++ src/page_scraper.go
@@ -11,11 +11,12 @@ import (
 
 type mal_member struct {
 	username string
+	score    string
+	status   string
+	eps_seen string
 }
 
-//  (status_code int, html_node *html.Node, err error)
-
-func Stats_scrape(page_html *html.Node) error {
+func Stats_scrape(page_html *html.Node) (member mal_member, err error) {
 	var stats_table_xpath string = "/html/body/div[1]/div[2]/div[3]/div[2]/table/tbody/tr/td[2]/div[1]/table[2]/tbody/tr"
 	var member_name_xpath string = "/td[1]/div[2]/a"
 	var member_score_xpath string = "/td[2]"
@@ -25,7 +26,7 @@ func Stats_scrape(page_html *html.Node) error {
 	member_row, err := htmlquery.QueryAll(page_html, stats_table_xpath)
 	if err != nil {
 		log.Error().Err(err).Msg("Error getting stats table")
-		return err
+		return member, err
 	}
 
 	for _, element := range member_row[1:] {
@@ -34,7 +35,7 @@ func Stats_scrape(page_html *html.Node) error {
 		name_node, err := htmlquery.Query(element, member_name_xpath)
 		if err != nil {
 			log.Error().Err(err).Msg("Error getting member name from stats table")
-			return err
+			return member, err
 		}
 		MAL_username_string := htmlquery.InnerText(name_node)
 
@@ -42,14 +43,14 @@ func Stats_scrape(page_html *html.Node) error {
 		score_node, err := htmlquery.Query(element, member_score_xpath)
 		if err != nil {
 			log.Error().Err(err).Msg("Error getting score from stats table")
-			return err
+			return member, err
 		}
 		MAL_score_string := htmlquery.InnerText(score_node)
 
 		status_node, err := htmlquery.Query(element, member_status_xpath)
 		if err != nil {
 			log.Error().Err(err).Msg("Error getting watch status from stats table")
-			return err
+			return member, err
 		}
 		MAL_status_string := htmlquery.InnerText(status_node)
 
@@ -57,7 +58,7 @@ func Stats_scrape(page_html *html.Node) error {
 		eps_node, err := htmlquery.Query(element, member_eps_seen_xpath)
 		if err != nil {
 			log.Error().Err(err).Msg("Error getting eps seen from stats table")
-			return err
+			return member, err
 		}
 		raw_eps_seen_string := htmlquery.InnerText(eps_node)
 
@@ -71,13 +72,18 @@ func Stats_scrape(page_html *html.Node) error {
 			MAL_eps_seen_string = "0"
 		}
 
-		log.Debug().Str("username", MAL_username_string).
-			Str("score", MAL_score_string).
-			Str("status", MAL_status_string).
-			Str("eps seen", MAL_eps_seen_string).
-			Msg("Member")
+		// log.Debug().Str("username", MAL_username_string).
+		// 	Str("score", MAL_score_string).
+		// 	Str("status", MAL_status_string).
+		// 	Str("eps seen", MAL_eps_seen_string).
+		// 	Msg("Member")
+
+		member.username = MAL_username_string
+		member.score = MAL_score_string
+		member.status = MAL_status_string
+		member.eps_seen = MAL_eps_seen_string
 	}
 
-	return nil
+	return member, nil
 
 }
