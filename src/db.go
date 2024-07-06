e4eb569816d60cddd6a89f20e7ea8adaf269a0c5
KanchiMoe <github@chiyo-chan.me>
2024-07-06 02:07:49 +0100

    load dotenv file with db settings

--- src/db.go
+++ src/db.go
@@ -4,11 +4,22 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
 
 func DB() (ret *pgxpool.Pool) {
 	var err error
+
+	// load dotenv
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Could not load .env file")
+	} else {
+		log.Trace().Msg("Was able to load .env file")
+	}
+
+	// make db connection
 	ctx := context.Background()
 	config, err := pgxpool.ParseConfig("")
 	if err != nil {
