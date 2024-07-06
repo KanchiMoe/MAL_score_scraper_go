4d91759ae17ded410a42843bce1b743301e4da85
KanchiMoe <github@chiyo-chan.me>
2024-07-06 01:51:18 +0100

    move to logic

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"os"
@@ -11,37 +10,14 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-const ROOTURL = "https://myanimelist.net"
-const MAX_OFFSET = 7425
-const SLEEP_PERIOD time.Duration = 1 * time.Second
-
 func main() {
 	project_config()
-	var offset int = 0
-	var stats_slug string = "/anime/6547/Angel_Beats/stats?show="
-
-	for offset <= MAX_OFFSET {
-		// create url
-		var current_url string = fmt.Sprintf("%s%s%d", ROOTURL, stats_slug, offset)
-
-		// request the page
-		var page_html = src.Request_handler(current_url)
-
-		// scrape the page
-		src.Stats_scrape(page_html)
-
-		// increase offset by 75
-		offset += 75
-
-		// sleep
-		log.Debug().Str("duration", SLEEP_PERIOD.String()).Msg("Sleeping")
-		time.Sleep(SLEEP_PERIOD)
-	}
+	src.Logic_main()
 }
 
 func project_config() {
 	// set log level
-	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
 	zerolog.DurationFieldUnit = time.Second
 }
