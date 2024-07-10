75694696644f0a4c33ec8ab830ba99fb530e7a44
KanchiMoe <github@chiyo-chan.me>
2024-07-11 00:02:05 +0100

    returns id from scrape

--- src/sql_is_id_in_db.go
+++ src/sql_is_id_in_db.go
@@ -2,7 +2,11 @@ package src
 
 import (
 	"fmt"
+	"net/url"
 
+	"strconv"
+
+	"github.com/antchfx/htmlquery"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/net/html"
@@ -23,15 +27,56 @@ func sql_is_id_in_db(_ *pgxpool.Pool, db_results mal_member) {
 	// request the url
 
 	page_html := Request_handler(profile_page)
-	_, err := scrape_member_page(page_html)
+	user_id, err := scrape_member_page(page_html)
 	if err != nil {
 		log.Error().Err(err).Msg("Error scraping member's page")
 		return
 	}
 
+	panic("foo")
+
 }
 
 func scrape_member_page(page_html *html.Node) (user_id int, err error) {
-	panic("foo111")
+	var report_link_xpath string = "/html/body/div[1]/div[2]/div[3]/div[1]/h1/a"
 
+	// get member's id
+	id_node, err := htmlquery.Query(page_html, report_link_xpath)
+	if err != nil {
+		log.Error().Err(err).Msg("Error getting member's id")
+		return 0, err
+	}
+	if id_node == nil {
+		log.Error().Err(err).Msg("ID node empty")
+		return 0, err
+	}
+
+	// get ahref
+	report_href := htmlquery.SelectAttr(id_node, "href")
+	if report_href == "" {
+		log.Error().Err(err).Msg("ahref node empty")
+		return 0, nil
+	}
+
+	// parse the url
+	parsed_url, err := url.Parse(report_href)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to parse URL")
+		return 0, err
+	}
+
+	// get ID from the url
+	user_id_string := parsed_url.Query().Get("id")
+	if user_id_string == "" {
+		log.Error().Err(err).Msg("No id parameter found in the URL")
+		return 0, nil
+	}
+
+	user_id_int, err := strconv.Atoi(user_id_string)
+	if err != nil {
+		log.Error().Str("string", user_id_string).Err(err).Msg("Unable to convert id string to int")
+		return 0, err
+	}
+
+	return user_id_int, nil
 }
